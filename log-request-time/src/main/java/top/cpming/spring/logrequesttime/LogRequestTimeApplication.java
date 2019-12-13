0826cf3ba7964f63dd0200cc22fc2f39fda9ed4e
caipeiming <394912998@qq.com>
2019-12-13 16:31:08 +0800

    增加记录请求运行时间项目

---
+++ log-request-time/src/main/java/top/cpming/spring/logrequesttime/LogRequestTimeApplication.java
@@ -0,0 +1,13 @@
+package top.cpming.spring.logrequesttime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogRequestTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogRequestTimeApplication.class, args);
+	}
+
+}
---
+++ log-request-time/src/test/java/top/cpming/spring/logrequesttime/LogRequestTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package top.cpming.spring.logrequesttime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogRequestTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
